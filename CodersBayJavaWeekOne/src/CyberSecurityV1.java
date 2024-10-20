b376a1c6c091f4971579a5ee06927a20181d9622
Bhety Penetzdorfer <bhety.tech@gmail.com>
2024-10-20 11:27:44 +0200

    Feat: Version 2 CyberSecurityV

--- CodersBayJavaWeekOne/src/CyberSecurityV1.java
+++ CodersBayJavaWeekOne/src/CyberSecurityV1.java
@@ -13,16 +13,15 @@ public class CyberSecurityV1 {
 
         int passwordMatches = 0;
         int totalGuess = 0;
-
         int highestAmountofGuess = 0;
 
-        int possibleAmountofGuess = (int) Math.pow(charLength, passwordLength);
-        int leastAmountofGuess = possibleAmountofGuess + 1;
+        int possibleGuesses = (int) Math.pow(charLength, passwordLength);
+        int leastAmountofGuess = possibleGuesses + 1;
 
 
 
-        for (int i = 0; i < 10; i++) {
-            int [] indexArr = new int[passwordLength];
+        for (int i = 0; i < 10; i++) { //for variable length password i = min value and i < max value
+            int [] indexArr = new int[passwordLength]; // here is the i for current password length
             StringBuilder password = new StringBuilder();
 
             //to generate a fixed length character password
@@ -31,15 +30,15 @@ public class CyberSecurityV1 {
                 password.append(allowedCharacter[randomIndex]);
             }
 
-            System.out.println("\nPassword to Match: " +password.toString());
+            System.out.println("\nPassword to Match: " +password);
 
 
             boolean isFound = false;
 
 
-            for (int x = 0 ; (x < possibleAmountofGuess) && !isFound ; x++) {
+            for (int x = 0 ; (x < possibleGuesses) && !isFound ; x++) {
 
-                //based N guessing the password
+                //based N guessing the password raise to the power of passwordLength UnitsPlace starts at 10° = 1;
                 for(int position = 0; position < passwordLength; position++)
                 {
                     indexArr[position] = (int) ((x / (Math.pow(charLength, position))) % charLength);
@@ -49,10 +48,11 @@ public class CyberSecurityV1 {
 //                int index3 = (int) ((x / (Math.pow(charLength, 2))) % charLength);
 //                int index4 = (int) ((x / (Math.pow(charLength, 3))) % charLength); //u.s.w ...
 
-                String guessPassword = "";
+                StringBuilder guessPassword = new StringBuilder();
                 for(int positions = passwordLength-1; positions >= 0 ; positions--)
                 {
-                    guessPassword += String.valueOf(allowedCharacter[indexArr[positions]]);
+                    guessPassword.append(allowedCharacter[indexArr[positions]]);
+                    // concatenated version guessPassword += String.valueOf(allowedCharacter[indexArr[positions]]);
                 }
 //                String guessPassword = String.valueOf(allowedCharacter[index4]) +
 //                        allowedCharacter[index3] +
@@ -60,7 +60,7 @@ public class CyberSecurityV1 {
 //                        allowedCharacter[index1];
 
 
-                if (guessPassword.contentEquals(password)) {
+                if (guessPassword.toString().contentEquals(password)) {
 
                     System.out.println("Generated Password: " + guessPassword + " = " + " Guess Password: " +guessPassword);
                     System.out.println("Password Match: " + guessPassword + " = " + password);
@@ -83,7 +83,9 @@ public class CyberSecurityV1 {
 
         }
 
-        System.out.println("\nPossible amount of Guesses: " + possibleAmountofGuess);
+
+        
+        System.out.println("\nPossible amount of Guesses: " + possibleGuesses);
         System.out.println("Total guesses: " + totalGuess);
         System.out.println("Password Matches: " + passwordMatches);
         System.out.println("The slowest guess is: " + highestAmountofGuess);
