8bb18bb547600071b96a0ae6474939de47c1f89a
Bhety Penetzdorfer <bhety.tech@gmail.com>
2024-10-22 10:59:39 +0200

    Fix: KinoVerwaltungV2 Final Version

--- CodersBayJavaWeekOne/src/KinoVerwaltungV2FinalVersion.java
+++ CodersBayJavaWeekOne/src/KinoVerwaltungV2FinalVersion.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class KinoVerwaltungV2Version2 {
+public class KinoVerwaltungV2FinalVersion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         final String adminEntry = "admin1234";
